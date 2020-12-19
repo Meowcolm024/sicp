db665a7af543438e4674106e4446e79d448d6845
Meowcolm024 <luoyicong1026@live.com>
2020-12-19 18:05:35 +0800

    aha

--- register-machine.rkt
+++ register-machine.rkt
@@ -45,9 +45,77 @@
                   (else (error "Unknown"))))
         dispatch)))
 
+(define (start machine) (machine 'start))
+(define (get-register-contents machine register-name)
+    (get-contents (get-register machine register-name)))
+(define (set-register-contents! machine register-name value)
+    (set-contenes! (get-register machine register-name) value) 'done)
+(define (get-register machine reg-name)
+    ((machine 'get-register) reg-name))
 
-(define assemble '())
-(define instruction-execution-proc '())
+(define (assemble controller-text machine)
+    (extract-labels
+        controller-text
+        (lambda (insts labels)
+            (update-insts! insts labels machine)
+            insts)))
+
+(define (extract-labels text receive)
+    (if (null? text)
+        (receive '() '())
+        (extract-labels
+            (cdr text)
+            (lambda (insts labels)
+                (let ((next-inst (car text)))
+                    (if (symbol? next-inst)
+                        (receive insts 
+                            (cons (make-label-entry next-inst insts) 
+                                    labels))
+                        (receive (cons (make-instruction next-inst) insts) 
+                                        labels)))))))
+
+(define (make-label-entry label-name insts) (cons label-name insts))
+
+(define (update-insts! insts labels machine) 
+    (let ((pc (get-register machine 'pc))
+          (flag (get-register machine 'flag))
+          (stack (machine 'stack))
+          (ops (machine 'operations)))
+        (for-each
+            (lambda (inst)
+                (set-instruction-execution-proc!
+                    inst
+                    (make-execution-procedure
+                        (instruction-text inst) labels machine pc flag stack ops)))
+            insts)))
+
+(define (make-instruction text) (cons text '()))
+(define (set-instruction-execution-proc! inst proc) (set-cdr! inst proc))
+(define instruction-text car)
+(define instruction-execution-proc cdr)
+
+(define (lookup-label labels label-name)
+    (let ((val (assoc label-name labels)))
+        (if val
+            (cdr val)
+            (error "Undefined"))))
+
+(define (make-execution-procedure inst labels machine pc flag stack ops)
+    (cond ((eq? (car inst) 'assign) 
+            (make-assign inst machine labels ops pc))
+          ((eq? (car inst) 'test)
+            (make-test inst machine labels ops flag pc))
+          ((eq? (car inst) 'branch)
+            (make-branch inst machine labels flag pc))
+          ((eq? (car inst) 'goto)
+            (make-goto inst machine labels pc))
+          ((eq? (car inst) 'save)
+            (make-save inst machine stack pc))
+          ((eq? (car inst) 'restore)
+            (make-restore inst machine stack pc))
+          ((eq? (car inst) 'perform)
+            (make-perform inst machine labels ops pc))
+          (else (error "Unknown"))))
 
 (define (make-machine register-names ops controller-text)
     (let ((machine (make-new-machine)))
